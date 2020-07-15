8015abfda44d5e35df8c63b1b03342f8f84ebbf7
nikhilbachani <nikhilbachani25@gmail.com>
2020-07-15 12:35:21 -0400

    First commit with folder strutcure

---
+++ private/initialize.php
@@ -0,0 +1,5 @@
+<?php
+
+	require_once('functions.php'); // load all code in functions.php, always use static strings here
+
+?>
---
+++ private/shared/staff_header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>GBI</title>
+		<meta charset="utf-8">
+		<link rel="stylesheet" media="all" type="text/css" href="../stylesheets/staff.css">
+	</head>
+	<body>
+		<header>
+			<h1>GBI Staff Area</h1>
+		</header>
+
+		<nav>
+			<ul>
+				<li><a href="index.php">Menu</a></li>
+			</ul>
+		</nav>
