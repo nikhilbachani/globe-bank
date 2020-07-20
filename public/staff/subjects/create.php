2f4be75e5e6f04db39c587e7d59f632e2c37f773
nikhilbachani <nikhilbachani25@gmail.com>
2020-07-20 13:36:00 -0400

    Added PHP script to handle subject creation

---
+++ public/staff/subjects/create.php
@@ -0,0 +1,13 @@
+<?php
+	// handle form values sent by new.php
+
+	$menu_name = $_POST['menu_name'] ?? '';
+	$position = $_POST['position'] ?? '';
+	$visible = $_POST['visible'] ?? '';	
+
+	echo 'Form parameters<br />';
+	echo 'Menu name: ' . $menu_name . '<br />';
+	echo 'Position: ' . $position . '<br />';
+	echo 'Visible: ' . $visible . '<br />';
+
+?>
