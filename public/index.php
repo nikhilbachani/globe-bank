e8aaddc8c4a72d9d20bb5f92ff4b90501fd9c858
nikhilbachani <nikhilbachani25@gmail.com>
2020-07-22 01:09:34 -0400

    Minor HTML changes to pass validation

--- public/index.php
+++ public/index.php
@@ -2,9 +2,9 @@
 <html lang="en">
 	<head>
 		<title>Globe Bank</title>
-		<meta charset="utf-8">
+		<meta charset="utf-8" />
 	</head>
 	<body>
-		<h1>Globe Bank: Coming Soon</h1<>
+		<h1>Globe Bank: Coming Soon</h1>
 	</body>
 </html>
