025db89d34dd65b76ec45fd621f96e44b7483c15
nikhilbachani <nikhilbachani25@gmail.com>
2020-07-27 18:11:09 -0400

    Added CRUD operations for pages and form options are now pulled from DB

--- private/query_functions.php
+++ private/query_functions.php
@@ -65,8 +65,8 @@
 		$sql = "DELETE FROM subjects ";
 		$sql .= "WHERE id = '" . $id . "' ";
 		$sql .= "LIMIT 1";
+
 		$result = mysqli_query($db, $sql);
-		
 		if (!$result) {
 			echo mysqli_error($db);
 			db_disconnect($db);
@@ -82,6 +82,74 @@
 		$result = mysqli_query($db, $sql);
 		confirm_result_set($result);
 		return $result;
+	}
+
+	function find_page_by_id($id) {
+		global $db;
+
+		$sql = "SELECT * FROM pages ";
+		$sql .= "WHERE id = '" . $id . "'";
+		$result = mysqli_query($db, $sql);
+		confirm_result_set($result);
+		$page = mysqli_fetch_assoc($result);
+		mysqli_free_result($result);
+		return $page;
+	}
+
+	function insert_page($page) {
+		global $db;
+
+		$sql = "INSERT INTO pages ";
+		$sql .= "(subject_id, menu_name, position, visible, content) ";
+		$sql .= "VALUES (";
+		$sql.= "'" . $page['subject_id'] . "', ";
+		$sql.= "'" . $page['menu_name'] . "', ";
+		$sql.= "'" . $page['position'] . "', ";
+		$sql.= "'" . $page['visible'] . "', ";
+		$sql.= "'" . $page['content'] . "'";
+		$sql .= ")";
+
+		$result = mysqli_query($db, $sql);
+		if (!$result) {
+			echo mysqli_error($db);
+			db_disconnect($db);
+			exit;
+		}
+	}
+
+	function update_page($page) {
+		global $db;
+
+		$sql = "UPDATE pages SET ";
+		$sql .= "subject_id = '" . $page['subject_id'] . "', ";
+		$sql .= "menu_name = '" . $page['menu_name'] . "', ";
+		$sql .= "position = '" . $page['position'] . "', ";
+		$sql .= "visible = '" . $page['visible'] . "', ";
+		$sql .= "content = '" . $page['content'] . "' ";
+		$sql .= "WHERE id = '" . $page['id'] . "' ";
+		$sql .= "LIMIT 1";
+
+		$result = mysqli_query($db, $sql);
+		if (!$result) {
+			echo mysqli_error($db);
+			db_disconnect($db);
+			exit;
+		}
+	}
+
+	function delete_page($id) {
+		global $db;
+
+		$sql = "DELETE FROM pages ";
+		$sql .= "WHERE id = '" . $id . "' ";
+		$sql .= "LIMIT 1";
+
+		$result = mysqli_query($db, $sql);
+		if (!$result) {
+			echo mysqli_error($db);
+			db_disconnect($db);
+			exit;
+		}
 	}
 
 ?>
