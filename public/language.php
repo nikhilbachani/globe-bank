a254ff88a2632fe370634633cd20560368b6f7af
nikhilbachani <nikhilbachani25@gmail.com>
2020-08-09 16:34:18 -0400

    Added language preferences page

---
+++ public/language.php
@@ -0,0 +1,42 @@
+<?php require_once('../private/initialize.php'); ?>
+
+<?php
+	if (is_post_request()) {
+		$language = $_POST['language'] ?? 'Any';
+		$expire = time() + 60 * 60 * 24 * 365;
+		setcookie('language', $language, $expire);
+	} else {
+		$language = $_COOKIE['language'] ?? 'None';
+	}
+?>
+
+<?php include(SHARED_PATH . '/public_header.php'); ?>
+
+<div id="main">
+	<?php include(SHARED_PATH . '/public_navigation.php'); ?>
+
+	<div id="page">
+		<div id="content">
+			<h1>Set Language Preference</h1>
+			<p>The currently selected language is: <?php echo $language; ?></p>
+			<form action="<?php echo url_for('/language.php'); ?>" method="post">
+				<select name="language">
+					<?php
+						$language_choices = ['Any', 'English', 'Spanish', 'French', 'German'];
+						foreach ($language_choices as $language_choice) {
+							echo "<option value=\"{$language_choice}\"";
+							if ($language_choice == $language) {
+								echo " selected";
+							}
+							echo ">{$language_choice}</option>";
+						}
+					?>
+				</select><br />
+				<br />
+				<input type="submit" value="Edit Preference" />
+			</form>
+		</div>
+	</div>
+</div>
+
+<?php include(SHARED_PATH . '/public_footer.php'); ?>
