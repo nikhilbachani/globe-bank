8ad9be447e8e5a13f52e478cf4fbde61bd9dcb95
nikhilbachani <nikhilbachani25@gmail.com>
2020-08-16 01:49:46 -0400

    Completed TODO: Nest pages resource inside subjects resource (Issue #7). Pages are no longer accessible as an independent list now.

--- public/staff/index.php
+++ public/staff/index.php
@@ -13,9 +13,6 @@
 				<a href="<?php echo url_for('/staff/subjects/index.php'); ?>">Subjects</a>
 			</li>
 			<li>
-				<a href="<?php echo url_for('/staff/pages/index.php'); ?>">Pages</a>
-			</li>
-			<li>
 				<a href="<?php echo url_for('/staff/admins/index.php'); ?>">Admins</a>
 			</li>
 		</ul>
